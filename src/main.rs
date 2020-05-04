3c25c2c5c84b6a06d51cd6d06d0fbbb09e9be68a
jad <jad@pml.ac.uk>
2020-05-04 16:34:06 +0100

    jad: precomputed variables bw, bbr and ay before calling dwcpn

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 //use crate::modules::time::gen_time_array;
 
 use crate::dwcpn::dwcpn::{InputParams, calc_pp};
+use crate::dwcpn::modules::pp_profile::{calculate_bw, calculate_bbr, calculate_ay};
 
 mod dwcpn;
 
 fn main() {
+
+    // pre calculate bw/bbr/ay arrays for use in the pp_profile calculation later
+    // this only needs to be done once for all pixels so I have it here, before we start
+    // looping over pixels
+    bw = calculate_bw();
+    bbr = calculate_bbr();
+    ay = calculate_ay();
+
 
     let input = InputParams{
         lat: 45.0,
@@ -21,7 +30,11 @@ fn main() {
         cloud: 0.0,
         yel_sub: 0.3,
         par: 400.0,
+        bw,
+        bbr,
+        ay
     };
+
 
     println!("pp = {:?}", calc_pp(input));
 
