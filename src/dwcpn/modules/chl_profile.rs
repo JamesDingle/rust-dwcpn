1228e0158c7aff3eabb1729b5c5f0a48653d1833
jad <jad@pml.ac.uk>
2020-05-04 10:16:58 +0100

    jad: removed potentially superfluous calculation of h as I think we are precomputing it now as part of the workspace file generation

--- src/dwcpn/modules/chl_profile.rs
+++ src/dwcpn/modules/chl_profile.rs
@@ -6,9 +6,11 @@ const TAU: f64 = PI * 2.0;
 pub fn gen_chl_profile(surface_chl: f64, sigma: f64, rho: f64, z_m: f64, h: f64) -> ([f64; DEPTH_PROFILE_COUNT], [f64; DEPTH_PROFILE_COUNT]) {
     let gauss_height = h / ( sigma * TAU.sqrt() );
     let b_0: f64 = surface_chl / (1.0 + (rho / (1.0 - rho)) * ( -z_m.powf(2.0) / (2.0 * sigma.powf(2.0)) ).exp() );
-    let h: f64 = sigma * (rho / (1.0 - rho)) * b_0 * TAU.sqrt();
-    let depth_array: [f64; DEPTH_PROFILE_COUNT] = gen_depth_array();
 
+    // research if we will always precompute h or keep the calculation in here
+    // let h: f64 = sigma * (rho / (1.0 - rho)) * b_0 * TAU.sqrt();
+
+    let depth_array: [f64; DEPTH_PROFILE_COUNT] = gen_depth_array();
 
     let mut chl_profile: [f64; DEPTH_PROFILE_COUNT] = [0.0; DEPTH_PROFILE_COUNT];
 
@@ -26,9 +28,6 @@ pub fn gen_chl_profile(surface_chl: f64, sigma: f64, rho: f64, z_m: f64, h: f64)
 
 fn gen_depth_array() -> [f64; DEPTH_PROFILE_COUNT] {
     let mut depth_array: [f64; DEPTH_PROFILE_COUNT] = [0.0; DEPTH_PROFILE_COUNT];
-
-    let count = DEPTH_PROFILE_COUNT as f64;
-    let step = DEPTH_PROFILE_STEP as f64;
 
     for i in 0..DEPTH_PROFILE_COUNT {
         let i_f64 = i as f64;
