3fb41154e27379a7aa4cab02c49179a83484b3b0
jad <jad@pml.ac.uk>
2020-12-08 16:18:49 +0000

    fixed time array generation such that it actually reaches mid-day now!

--- src/dwcpn/modules/time.rs
+++ src/dwcpn/modules/time.rs
@@ -2,8 +2,8 @@ use crate::dwcpn::modules::config::TIMESTEPS;
 
 pub fn generate_time_array(sunrise: f64) -> ([f64; TIMESTEPS], f64) {
     let mut time_array: [f64; TIMESTEPS] = [0.0; TIMESTEPS];
-    let count = TIMESTEPS as f64;
-    let delta_t: f64 = (12.0 - sunrise) / count;
+    let count = (TIMESTEPS) as f64;
+    let delta_t: f64 = (12.0 - sunrise) / (count - 1.0);
 
     for i in 0..TIMESTEPS {
         let i_f64 = i as f64;
@@ -13,6 +13,21 @@ pub fn generate_time_array(sunrise: f64) -> ([f64; TIMESTEPS], f64) {
     return (time_array, delta_t);
 }
 
+// pub fn generate_time_array_zenith(zenith_time: f64) -> ([f64; TIMESTEPS], f64) {
+//     let mut time_array: [f64; TIMESTEPS] = [0.0; TIMESTEPS];
+//     let count = (TIMESTEPS + 1) as f64;
+//     let delta_t: f64 = (12.0 - zenith_time) / count;
+//
+//     for i in 0..TIMESTEPS {
+//         let i_f64 = i as f64;
+//         time_array[i] = zenith_time + delta_t * i_f64;
+//     }
+//
+//     return (time_array, delta_t);
+// }
+
+
+
 pub fn compute_sunrise(jday: u16, lat: f64) -> (f64, f64, f64) {
     // convert julian day to double for the equation
     let dayfloat = jday as f64;
@@ -21,7 +36,8 @@ pub fn compute_sunrise(jday: u16, lat: f64) -> (f64, f64, f64) {
 
     let theta: f64 = tau * dayfloat / 365.0f64;
 
-    let delta: f64 = 0.006918 - 0.399912 * theta.cos() + 0.070257 * theta.sin()
+    let delta: f64 = 0.006918 - 0.399912 * theta.cos()
+        + 0.070257 * theta.sin()
         - 0.006758 * (2.0 * theta).cos()
         + 0.000907 * (2.0 * theta).sin()
         - 0.002697 * (3.0 * theta).cos()
